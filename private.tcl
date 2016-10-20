b0e89adc472a6401c97e34c1937f6b3dc5295d66
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-10-19 20:59:37 -0400

    Updated steps to untrack private.tcl.

--- private.tcl
+++ private.tcl
@@ -1,5 +1,5 @@
 # To untrack this file use the following command before editing:
-# git update-index --assume-unchanged example/private.tcl
+# git update-index --assume-unchanged private.tcl
 
 set ownerId 000000000000000000
 set token "Replace with your token."
